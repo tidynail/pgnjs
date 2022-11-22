7d32ac28221c0c8960288539c091c72e7aaa9a65
Young-Jo Song <tidynail@gmail.com>
2022-11-21 23:57:06 -0500

    index.d.ts prepration

--- index.d.ts
+++ index.d.ts
@@ -1,78 +1,126 @@
-declare module "pgn.js" {
-
-export type Games = Game[];
-
-export interface Tag {
-  name: string,
-  value: string,
-};
-
-export interface Move {
-  // from chess.js
-  color: string, /* 'w', 'b' */
-  from: string,
-  to: string,
-  flags: string,
-    /*
-    'n' - a non-capture
-    'b' - a pawn push of two squares
-    'e' - an en passant capture
-    'c' - a standard capture
-    'p' - a promotion
-    'k' - kingside castling
-    'q' - queenside castling
-    */
-  piece: string, /* p, b, n, r, q, k */
-  san: string,
-  captured?: string,
-  promotion?: string,
-
-  // extends by pgn.js
-  num: number,            // move number, not ply
-  fen: string,
-  uci: string,            // uci long algebraic notation
-
-  comment?: {
-    pre?: string,         // comment before move number
-    before?: string,      // comment before san
-    after?: string        // comment after san and nag
-  },
-  nag?: string,           // $<number>
-
-  vars?: Move[][],
-
-  // status
-  over?: {
-    mate?: boolean,       // checkmate
-    draw?: string,        // stale, 3fold, fifty, material
-  },
-  check?: boolean,        // check by this move
-
-  ply: number,
-  line: Move[],         // array containing this move
-  prev: Move,           // previous move
-};
-
-export interface Options {
-  verbose: boolean, // print error message
-  onGame: (game: Game, error: Error) => void,
-  onFinish: () => void,
-};
-
-export interface Error {
-  msg?: string,
-  fen?: string,
-  san?: string,
-  num?: number,
-  movetext?: string,
-
-  // from parser
-  found?: string,   // unexpected token
-  location: {
-    start: { offset: number, line: number, column: number},
-    end: { offset: number, line: number, column: number}
-  }    
-};
-
-} // declare module
-
+declare module "util" {
+    export class Util {
+        static fill_move(move: any, chess: any): void;
+        static move_num_from_fen(fen: any): number;
+    }
+}
+declare module "game" {
+    export const NOVAR: "novar";
+    export namespace VAR {
+        const remove: string;
+        const replace: string;
+        const nextvar: string;
+        const lastvar: string;
+    }
+    export class Game {
+        tags: any[];
+        moves: any[];
+        gtm: any;
+        /**
+         * @return {string | undefined} fen
+         */
+        setupFen(): string | undefined;
+        /**
+         * @param {string} fen
+         */
+        setFen(fen: string): void;
+        /**
+         * @param {string} name
+         * @param {string} value
+         */
+        setTag(_name: any, _value: any): void;
+        /**
+         * @param {string} name
+         */
+        delTag(_name: any): void;
+        pgn(): string;
+        _moves_to_pgn(ctx: any, moves: any): void;
+        /**
+         * @param {string} san
+         * @param {Move[]?} line to add
+         * @return {Move | null}
+         */
+        move(san: string, line: Move[] | null): Move | null;
+        /**
+         * as the last variation
+         * @param {string} san
+         * @param {Move?} prev first move if null
+         * @return {Move | null}
+         */
+        var(san: string, prev?: Move): Move | null;
+        /**
+         * make others as variations
+         * @param {string} san
+         * @param {Move?} prev first move if null
+         * @return {Move | null}
+         */
+        main(san: string, prev?: Move): Move | null;
+        /**
+         * as the very next variation
+         * @param {string} san
+         * @param {Move?} prev first move if null
+         * @return {Move | null}
+         */
+        next(san: string, prev?: Move): Move | null;
+        /**
+         * no variation, overwrite
+         * @param {string} san
+         * @param {Move?} prev first move if null
+         * @return {Move | null}
+         */
+        replace(san: string, prev?: Move): Move | null;
+        _make_move(san: any, prev: any): import("chess.js").Move;
+    }
+}
+declare module "pgnparser" {
+    function peg$SyntaxError(message: any, expected: any, found: any, location: any): any;
+    class peg$SyntaxError {
+        constructor(message: any, expected: any, found: any, location: any);
+        format(sources: any): string;
+    }
+    namespace peg$SyntaxError {
+        function buildMessage(expected: any, found: any): string;
+    }
+    function peg$parse(input: any, options: any): any;
+    export { peg$SyntaxError as SyntaxError, peg$parse as parse };
+}
+declare module "pgn" {
+    export class Pgn {
+        /**
+         * @param {string} path stdin if ''
+         * @param {Options} opts
+         * @return {Pgn} pgn
+         */
+        static load(path: string, opts?: Options): Pgn;
+        /**
+         * @param {string} pgn
+         * @param {Options} opts
+         */
+        constructor(pgn?: string, opts?: Options);
+        games: any[];
+        /**
+         * @return {number}
+         */
+        count(): number;
+        /**
+         * @param {number} idx
+         * @return {Game}
+         */
+        game(idx: number): Game;
+        /**
+         * @return {Game}
+         */
+        newgame(): Game;
+        pgn(): string;
+        _from_pgn(pgn?: string, opts?: {}): any[];
+        _from_file(path: any, opts?: {}): Promise<any[]>;
+        _handle_line(ctx: any, line: any, opts: any): Promise<void>;
+        _parse_movetext(game: any, movetext: any, fen?: string, opts?: {}): any;
+        /**
+         * @return {error}
+         */
+        _make_moves(game: any, parent: any, parsed_moves: any, fen: any, prev_move: any, ply: any): error;
+    }
+    import { Game } from "game";
+}
+//# sourceMappingURL=index.d.ts.map
