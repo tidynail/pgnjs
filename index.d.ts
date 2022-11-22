97a210c94d80b9aadaa8d0c0e1e4200d621f9311
Young-Jo Song <tidynail@gmail.com>
2022-11-22 00:56:21 -0500

    add types.js

--- index.d.ts
+++ index.d.ts
@@ -47,28 +47,28 @@ declare module "game" {
          * @param {Move?} prev first move if null
          * @return {Move | null}
          */
-        var(san: string, prev?: Move): Move | null;
+        var(san: string, prev?: Move | null): Move | null;
         /**
          * make others as variations
          * @param {string} san
          * @param {Move?} prev first move if null
          * @return {Move | null}
          */
-        main(san: string, prev?: Move): Move | null;
+        main(san: string, prev?: Move | null): Move | null;
         /**
          * as the very next variation
          * @param {string} san
          * @param {Move?} prev first move if null
          * @return {Move | null}
          */
-        next(san: string, prev?: Move): Move | null;
+        next(san: string, prev?: Move | null): Move | null;
         /**
          * no variation, overwrite
          * @param {string} san
          * @param {Move?} prev first move if null
          * @return {Move | null}
          */
-        replace(san: string, prev?: Move): Move | null;
+        replace(san: string, prev?: Move | null): Move | null;
         _make_move(san: any, prev: any): import("chess.js").Move;
     }
 }
@@ -123,4 +123,132 @@ declare module "pgn" {
     }
     import { Game } from "game";
 }
+type Tag = {
+    name: string;
+    valuee: string;
+};
+/**
+ * from chess.js
+ */
+type Move = {
+    /**
+     * 'w', 'b'
+     */
+    color: string;
+    /**
+     * square
+     */
+    from: string;
+    /**
+     * square
+     */
+    to: string;
+    /**
+     * 'n' - a non-capture
+     * 'b' - a pawn push of two squares
+     * 'e' - an en passant capture
+     * 'c' - a standard capture
+     * 'p' - a promotion
+     * 'k' - kingside castling
+     * 'q' - queenside castling
+     */
+    flags: string;
+    /**
+     * p, b, n, r, q, k
+     */
+    piece: string;
+    /**
+     * SAN notation
+     */
+    san: string;
+    captured?: string | undefined;
+    /**
+     * extended by pgn.js
+     */
+    promotion?: string | undefined;
+    /**
+     * move number, not ply
+     */
+    num: number;
+    /**
+     * position fen
+     */
+    fen: string;
+    /**
+     * uci long algerbraic notation
+     */
+    uci: string;
+    comment?: any | undefined;
+    /**
+     * comment before move number
+     */
+    pre?: string | undefined;
+    /**
+     * comment before san
+     */
+    before?: string | undefined;
+    /**
+     * comment after san and nag
+     */
+    after?: string | undefined;
+    nags: string[];
+    /**
+     * variations (RAV)
+     */
+    vars: Move[][];
+    over?: any | undefined;
+    /**
+     * checkmate
+     */
+    mate?: boolean | undefined;
+    /**
+     * 'stale', '3fold', 'fifty', 'material'
+     */
+    draw?: string | undefined;
+    ply: number;
+    /**
+     * the line contaning this move
+     */
+    line: Move[];
+    /**
+     * previous move
+     */
+    prev: Move;
+};
+type Err = {
+    msg?: string | undefined;
+    fen?: string | undefined;
+    san?: string | undefined;
+    num?: number | undefined;
+    /**
+     * from parser
+     */
+    movetext?: string | undefined;
+    /**
+     * unexpected token
+     */
+    found?: string | undefined;
+    location?: any | undefined;
+    start: {
+        offset: number;
+        line: number;
+        column: number;
+    };
+    offset: number;
+    line: number;
+    column: number;
+};
+type OnGame = (game: Game, error: Err) => void;
+type OnFinish = () => void;
+type Options = {
+    /**
+     * print error message
+     */
+    verbose?: boolean | undefined;
+    /**
+     * called when a game is parsed
+     */
+    onGame: OnGame;
+    onFinish: OnFinishe;
+};
 //# sourceMappingURL=index.d.ts.map
---
+++ src/types.js
@@ -0,0 +1,84 @@
+/**
+ * @typedef {Object} Tag
+ * @prop {string} name
+ * @prop {string} valuee
+ */
+
+/**
+ * @typedef {Object} Move
+ * 
+ * from chess.js
+ * @prop {string} color 'w', 'b'
+ * @prop {string} from  square
+ * @prop {string} to    square
+ * @prop {string} flags
+ * 'n' - a non-capture
+ * 'b' - a pawn push of two squares
+ * 'e' - an en passant capture
+ * 'c' - a standard capture
+ * 'p' - a promotion
+ * 'k' - kingside castling
+ * 'q' - queenside castling
+ * @prop {string} piece  p, b, n, r, q, k
+ * @prop {string} san    SAN notation
+ * @prop {string=} captured
+ * @prop {string=} promotion
+ * 
+ * extended by pgn.js
+ * @prop {number} num     move number, not ply
+ * @prop {string} fen     position fen
+ * @prop {string} uci     uci long algerbraic notation
+ * @prop {Object=} comment
+ * @prop {string=} comment.pre      comment before move number
+ * @prop {string=} comment.before   comment before san
+ * @prop {string=} comment.after    comment after san and nag
+
+ * @prop {string[]} nags
+ * @prop {Move[][]} vars      variations (RAV)
+ * @prop {Object=} over
+ * @prop {boolean=} over.mate checkmate
+ * @prop {string=} over.draw  'stale', '3fold', 'fifty', 'material'
+
+ * @prop {number} ply
+ * @prop {Move[]} line    the line contaning this move
+ * @prop {Move} prev      previous move
+ */
+
+/**
+ * @typedef {Object} Err
+ * @prop {string=} msg
+ * @prop {string=} fen
+ * @prop {string=} san
+ * @prop {number=} num
+ * @prop {string=} movetext
+ * 
+ * from parser
+ * @prop {string=} found  unexpected token
+ * @prop {Object=} location
+ * @prop {Object} location.start
+ * @prop {number} location.start.offset
+ * @prop {number} location.start.line
+ * @prop {number} location.start.column
+ * @prop {number} location.end.offset
+ * @prop {number} location.end.line
+ * @prop {number} location.end.column
+ */
+
+/**
+ * @callback OnGame
+ * @param {Game} game   parsed game
+ * @param {Err} error   error
+ * @return {void}
+ */
+
+/**
+ * @callback OnFinish
+ * @return {void}
+ */
+
+/**
+ * @typedef {Object} Options
+ * @prop {boolean=} verbose print error message
+ * @prop {OnGame} onGame    called when a game is parsed
+ * @prop {OnFinishe} onFinish
+ */
