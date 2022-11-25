fb648a6966e65cf82407f07168324f7194acef68
tidynail <tidynail@gmail.com>
2022-11-25 17:02:09 -0500

    type png -> pgn

--- sample/sample.js
+++ sample/sample.js
@@ -3,7 +3,7 @@ const pgn = new Pgn(
 `[Event "Sample"]
 [Site "Toronto"]
 [Date "2022.11.17"]
-{This is a sample game for Png.js}
+{This is a sample game for Pgn.js}
 1.d4 (1.e4 c5 (1...e5 Bc4) (1...e6)) d5 {move comment}
 2.c4! +- (2.Nf3 {variation move comment} Nf6)
 (2. {comment before a move} Bf4) 2...c6 Nf3`);
